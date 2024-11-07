445eb3c71211b803311472cfb4af5b890008c31a
tanajungs28 <jun.9rs@gmail.com>
2024-11-07 23:58:51 +0900

    ダメージが出た際にキャラクターに点滅のアニメーションを追加、にげるコマンドの動きを追加

--- js/test.js
+++ js/test.js
@@ -23,9 +23,13 @@ var output2 = "たなじゅんはダメージをうけた！";
 var output3 = "あいこです";
 const myimage = document.querySelector("#myhand");
 const yourimage = document.querySelector("#yourhand");
+// const aite = document.querySelector("pic_aite");
 const music = document.querySelector("#bgm_fight");
+music.volume = 0.3;
 const button_sound = document.querySelector("#bgm_button");
 const win_sound = document.querySelector("#bgm_win");
+const lose_sound = document.querySelector("#bgm_lose");
+const damege_sound = document.querySelector("#bgm_damege");
 
 //モーダル画面のフェードアウトと登場人物のフェードイン
 $(function () {
@@ -49,6 +53,7 @@ $("button.Abutton").on("click", function(){
     if(n === 1){
         $("h2.exp").html(exp1);
     }else if(n === 2){
+        $("h2.your_output").html("");
         $("h2.exp").html(exp2);
     }else if(n === 3){
         $("h2.command1").html(choose+command1);
@@ -220,6 +225,7 @@ $("button.Abutton").on("click", function(){
             if(your_hand === 0){
                 $("h2.your_output").html("あいては「グー」を出してきた。");
                 yourimage.src = "./img/hand_gu.png";
+                damege_sound.play();
                 $("h2.exp").html(output3);
                 $("h2.command1").html("");
                 $("h2.command2").html("");
@@ -230,6 +236,7 @@ $("button.Abutton").on("click", function(){
             }else if(your_hand === 1){
                 $("h2.your_output").html("あいては「チョキ」を出してきた。");
                 yourimage.src = "./img/hand_tyoki.png";
+                damege_sound.play();
                 $("h2.exp").html(output1);
                 $("h2.command1").html("");
                 $("h2.command2").html("");
@@ -237,11 +244,24 @@ $("button.Abutton").on("click", function(){
                 $("h2.command4").html("");
                 your_percent = your_percent -30 ;
                 $('.your_health_guage').css('width',your_percent);
+                //ダメージ与えた際の点滅処理
+                var i = 0;
+                $(function(){
+                        var id = setInterval(function(){
+                            $('.pic_aite').fadeOut(100, function(){$(this).fadeIn(100)});
+                            i++;
+                            if(i === 5){
+                                clearInterval(id);
+                                console.log("iの値：",i)
+                            };
+                        }, 100);
+                    });                    
                 console.log("自分の体力",my_percent)
                 console.log("相手の体力",your_percent)
             }else if(your_hand === 2){
                 $("h2.your_output").html("あいては「パー」を出してきた。");
                 yourimage.src = "./img/hand_pa.png";
+                damege_sound.play();
                 $("h2.exp").html(output2);
                 $("h2.command1").html("");
                 $("h2.command2").html("");
@@ -249,6 +269,18 @@ $("button.Abutton").on("click", function(){
                 $("h2.command4").html("");
                 my_percent = my_percent -30;
                 $('.my_health_guage').css('width',my_percent);
+                //ダメージ与えた際の点滅処理
+                var i = 0;
+                $(function(){
+                        var id = setInterval(function(){
+                            $('.pic_jibun').fadeOut(100, function(){$(this).fadeIn(100)});
+                            i++;
+                            if(i === 5){
+                                clearInterval(id);
+                                console.log("iの値：",i)
+                            };
+                        }, 100);
+                    });                    
                 console.log("自分の体力",my_percent)
                 console.log("相手の体力",your_percent)
             }
@@ -262,6 +294,7 @@ $("button.Abutton").on("click", function(){
             if(your_hand === 0){
                 $("h2.your_output").html("あいては「グー」を出してきた。");
                 yourimage.src = "./img/hand_gu.png";
+                damege_sound.play();
                 $("h2.exp").html(output2);
                 $("h2.command1").html("");
                 $("h2.command2").html("");
@@ -269,11 +302,24 @@ $("button.Abutton").on("click", function(){
                 $("h2.command4").html("");
                 my_percent = my_percent -30;
                 $('.my_health_guage').css('width',my_percent);
+                                //ダメージ与えた際の点滅処理
+                                var i = 0;
+                                $(function(){
+                                        var id = setInterval(function(){
+                                            $('.pic_jibun').fadeOut(100, function(){$(this).fadeIn(100)});
+                                            i++;
+                                            if(i === 5){
+                                                clearInterval(id);
+                                                console.log("iの値：",i)
+                                            };
+                                        }, 100);
+                                    });                                    
                 console.log("自分の体力",my_percent)
                 console.log("相手の体力",your_percent)
             }else if(your_hand === 1){
                 $("h2.your_output").html("あいては「チョキ」を出してきた。");
                 yourimage.src = "./img/hand_tyoki.png";
+                damege_sound.play();
                 $("h2.exp").html(output3);
                 $("h2.command1").html("");
                 $("h2.command2").html("");
@@ -284,6 +330,7 @@ $("button.Abutton").on("click", function(){
             }else if(your_hand === 2){
                 $("h2.your_output").html("あいては「パー」を出してきた。");
                 yourimage.src = "./img/hand_pa.png";
+                damege_sound.play();
                 $("h2.exp").html(output1);
                 $("h2.command1").html("");
                 $("h2.command2").html("");
@@ -291,6 +338,18 @@ $("button.Abutton").on("click", function(){
                 $("h2.command4").html("");
                 your_percent = your_percent -30 ;
                 $('.your_health_guage').css('width',your_percent);
+                                //ダメージ与えた際の点滅処理
+                                var i = 0;
+                                $(function(){
+                                        var id = setInterval(function(){
+                                            $('.pic_aite').fadeOut(100, function(){$(this).fadeIn(100)});
+                                            i++;
+                                            if(i === 5){
+                                                clearInterval(id);
+                                                console.log("iの値：",i)
+                                            };
+                                        }, 100);
+                                    });                
                 console.log("自分の体力",my_percent)
                 console.log("相手の体力",your_percent)
             }
@@ -304,6 +363,7 @@ $("button.Abutton").on("click", function(){
             if(your_hand === 0){
                 $("h2.your_output").html("あいては「グー」を出してきた。");
                 yourimage.src = "./img/hand_gu.png";
+                damege_sound.play();
                 $("h2.exp").html(output1);
                 $("h2.command1").html("");
                 $("h2.command2").html("");
@@ -311,11 +371,24 @@ $("button.Abutton").on("click", function(){
                 $("h2.command4").html("");
                 your_percent = your_percent -30 ;
                 $('.your_health_guage').css('width',your_percent);
+                                //ダメージ与えた際の点滅処理
+                                var i = 0;
+                                $(function(){
+                                        var id = setInterval(function(){
+                                            $('.pic_aite').fadeOut(100, function(){$(this).fadeIn(100)});
+                                            i++;
+                                            if(i === 5){
+                                                clearInterval(id);
+                                                console.log("iの値：",i)
+                                            };
+                                        }, 100);
+                                    });                
                 console.log("自分の体力",my_percent)
                 console.log("相手の体力",your_percent)
             }else if(your_hand === 1){
                 $("h2.your_output").html("あいては「チョキ」を出してきた。");
                 yourimage.src = "./img/hand_tyoki.png";
+                damege_sound.play();
                 $("h2.exp").html(output2);
                 $("h2.command1").html("");
                 $("h2.command2").html("");
@@ -323,11 +396,24 @@ $("button.Abutton").on("click", function(){
                 $("h2.command4").html("");
                 my_percent = my_percent -30;
                 $('.my_health_guage').css('width',my_percent);
+                                //ダメージ与えた際の点滅処理
+                                var i = 0;
+                                $(function(){
+                                        var id = setInterval(function(){
+                                            $('.pic_jibun').fadeOut(100, function(){$(this).fadeIn(100)});
+                                            i++;
+                                            if(i === 5){
+                                                clearInterval(id);
+                                                console.log("iの値：",i)
+                                            };
+                                        }, 100);
+                                    });                                    
                 console.log("自分の体力",my_percent)
                 console.log("相手の体力",your_percent)
             }else if(your_hand === 2){
                 $("h2.your_output").html("あいては「パー」を出してきた。");
                 yourimage.src = "./img/hand_pa.png";
+                damege_sound.play();
                 $("h2.exp").html(output3);
                 $("h2.command1").html("");
                 $("h2.command2").html("");
@@ -336,6 +422,18 @@ $("button.Abutton").on("click", function(){
                 console.log("自分の体力",my_percent)
                 console.log("相手の体力",your_percent)
             }
+        }
+        if(m === 4){
+            console.log("自分の手：にげる")
+            console.log("mの中身：",m)
+            $("h2.your_output").html("、、、にげられなかった。");
+            $("h2.exp").html("");
+            $("h2.command1").html("");
+            $("h2.command2").html("");
+            $("h2.command3").html("");
+            $("h2.command4").html("");
+            n = 1;
+            m = 1; //次回のコマンド選択を初期位置（グー）にするためのリセット
         }
     }
     else if(n === 5 && my_percent > 0 && your_percent > 0){
@@ -349,7 +447,10 @@ $("button.Abutton").on("click", function(){
             }else if(n === 5 && my_percent < 0){
                 $("h2.exp").html("あなたの負けです");
                 $("h2.your_output").html("");
+                myimage.src = "";
+                yourimage.src = "";
                 music.pause();
+                lose_sound.play();
                 $('.pic_jibun').animate({
                     top: "+=50",
                     opacity: 0
@@ -359,6 +460,8 @@ $("button.Abutton").on("click", function(){
             }else if(n === 5 && your_percent < 0){
                 $("h2.exp").html("あなたの勝ちです");
                 $("h2.your_output").html("");
+                myimage.src = "";
+                yourimage.src = "";
                 music.pause();
                 win_sound.play();
                 $('.pic_aite').animate({
